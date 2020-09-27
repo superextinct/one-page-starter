7870f4db85e89a7d1d0038e2fb7be869ae6dfb94
Niklas <desk@niklassonnenschein.de>
2020-09-27 14:13:52 +0200

    first commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
+import "intersection-observer";
+import scrollama from "scrollama";
+import * as d3 from "d3";
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+const scroller = scrollama();
+
+scroller
+  .setup({
+    step: ".slide"
+  })
+  .onStepEnter(response => {
+    console.log(response);
+  })
+  .onStepExit(response => {
+    console.log(response);
+  });
+
+window.addEventListener("resize", scroller.resize);
